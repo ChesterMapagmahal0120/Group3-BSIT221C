90d3615286e7a5599b9f71e78560b082dae7b082
ChesterMapagmahal0120 <161694981+ChesterMapagmahal0120@users.noreply.github.com>
2024-03-03 00:55:15 +0800

    updated app.js
    I added Layout on components

--- recipe-app-group3/pages/_app.js
+++ recipe-app-group3/pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import 'semantic-ui-css/semantic.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  ) 
 }
