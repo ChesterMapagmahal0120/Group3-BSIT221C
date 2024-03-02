16fde3836970d4ec1160561dce631a6594512b25
ChesterMapagmahal0120 <161694981+ChesterMapagmahal0120@users.noreply.github.com>
2024-03-03 00:55:54 +0800

    Updated index
    Updated Home

--- recipe-app-group3/pages/index.js
+++ recipe-app-group3/pages/index.js
@@ -1,5 +1,13 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import React from "react";
+import Features from "@/components/Features";
+import Abouts from "@/components/Abouts";
+import SearchRecipes from "@/components/SearchRecipes"
+import Navbars from "@/components/Navbar";
+
+
+
 
 export default function Home() {
   return(
@@ -9,10 +17,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="#" />
       </Head>
-
       <main className={`${styles.main}`}>
-        
+       
       </main>
+      <Features/>
+      <Abouts/>
+      <SearchRecipes/>
     </>
   );
 }
